e582856d51ed855739aa395605c732ec8827ba7a
Lysander <lysander6@gmail.com>
2024-12-03 18:03:53 +0100

    Day 3, part 2

--- day_03/src/lib.rs
+++ day_03/src/lib.rs
@@ -38,7 +38,11 @@ impl<'a> ProgramParser<'a> {
         let mut result = vec![];
 
         while self.iterator.peek().is_some() {
-            if self.parse_mul() {
+            if self.parse_do() {
+                result.push(Instruction::Do);
+            } else if self.parse_dont() {
+                result.push(Instruction::Dont);
+            } else if self.parse_mul() {
                 if let Some('(') = self.iterator.peek() {
                     let _ = self.iterator.next();
 
@@ -64,13 +68,73 @@ impl<'a> ProgramParser<'a> {
         result
     }
 
+    fn parse_do(&mut self) -> bool {
+        let mut iter = self.iterator.clone();
+
+        if let Some('d') = iter.peek() {
+            let _ = iter.next();
+            if let Some('o') = iter.peek() {
+                let _ = iter.next();
+                if let Some('(') = iter.peek() {
+                    let _ = iter.next();
+                    if let Some(')') = iter.peek() {
+                        let _ = iter.next();
+
+                        self.iterator = iter;
+
+                        return true;
+                    }
+                }
+            }
+        }
+
+        false
+    }
+
+    fn parse_dont(&mut self) -> bool {
+        let mut iter = self.iterator.clone();
+
+        if let Some('d') = iter.peek() {
+            let _ = iter.next();
+            if let Some('o') = iter.peek() {
+                let _ = iter.next();
+                if let Some('n') = iter.peek() {
+                    let _ = iter.next();
+                    if let Some('\'') = iter.peek() {
+                        let _ = iter.next();
+                        if let Some('t') = iter.peek() {
+                            let _ = iter.next();
+                            if let Some('(') = iter.peek() {
+                                let _ = iter.next();
+                                if let Some(')') = iter.peek() {
+                                    // 🤡
+                                    let _ = iter.next();
+
+                                    self.iterator = iter;
+
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        false
+    }
+
     fn parse_mul(&mut self) -> bool {
-        if let Some('m') = self.iterator.peek() {
-            let _ = self.iterator.next();
-            if let Some('u') = self.iterator.peek() {
-                let _ = self.iterator.next();
-                if let Some('l') = self.iterator.peek() {
-                    let _ = self.iterator.next();
+        let mut iter = self.iterator.clone();
+
+        if let Some('m') = iter.peek() {
+            let _ = iter.next();
+            if let Some('u') = iter.peek() {
+                let _ = iter.next();
+                if let Some('l') = iter.peek() {
+                    let _ = iter.next();
+
+                    self.iterator = iter;
 
                     return true;
                 }
@@ -122,17 +186,54 @@ pub fn solve_part_1(p: &Problem) -> u64 {
     result
 }
 
+#[must_use]
+pub fn solve_part_2(p: &Problem) -> u64 {
+    let Problem { program } = p;
+    let mut parser = ProgramParser::new(program);
+    let program = parser.parse();
+
+    let mut mul_enabled = true;
+    let mut result = 0;
+
+    for instr in program {
+        match instr {
+            Instruction::Do => {
+                mul_enabled = true;
+            }
+            Instruction::Dont => {
+                mul_enabled = false;
+            }
+            Instruction::Mul(a, b) => {
+                if mul_enabled {
+                    result += a * b;
+                }
+            }
+        }
+    }
+
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const TEST_INPUT: &str =
-        "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-
     #[test]
     fn test_solve_part_1() {
-        let p: Problem = TEST_INPUT.parse().unwrap();
+        let p: Problem = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+            .parse()
+            .unwrap();
 
         assert_eq!(solve_part_1(&p), 161);
+    }
+
+    #[test]
+    fn test_solve_part_2() {
+        let p: Problem =
+            "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+                .parse()
+                .unwrap();
+
+        assert_eq!(solve_part_2(&p), 48);
     }
 }
