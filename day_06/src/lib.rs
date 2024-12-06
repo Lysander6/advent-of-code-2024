1965f77fe023d3edf29ad791a8438f5c1453566c
Lysander <lysander6@gmail.com>
2024-12-06 13:42:46 +0100

    Store map (obstacles) in a `HashMap`

--- day_06/src/lib.rs
+++ day_06/src/lib.rs
@@ -8,7 +8,9 @@ use std::{collections::HashSet, str::FromStr};
 
 #[derive(Debug)]
 pub struct Problem {
-    map: Vec<Vec<bool>>,
+    map_height: usize,
+    map_width: usize,
+    obstacles: HashSet<(usize, usize)>,
     starting_position: (usize, usize),
 }
 
@@ -48,39 +50,46 @@ impl FromStr for Problem {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut starting_position = (0, 0);
 
-        let map = s
+        let map_height = s.lines().count();
+        let map_width = s.lines().next().unwrap().chars().count();
+
+        let obstacles = s
             .lines()
             .enumerate()
-            .map(|(x, l)| {
+            .flat_map(|(x, l)| {
                 l.chars()
                     .enumerate()
-                    .map(|(y, c)| match c {
+                    .filter_map(|(y, c)| match c {
                         '^' => {
                             starting_position = (x, y);
-                            false
+                            None
                         }
-                        '.' => false,
-                        '#' => true,
+                        '.' => None,
+                        '#' => Some((x, y)),
                         _ => unreachable!("Unknown map symbol"),
                     })
                     .collect::<Vec<_>>()
             })
-            .collect::<Vec<_>>();
+            .collect::<HashSet<_>>();
 
         Ok(Problem {
-            map,
+            map_height,
+            map_width,
+            obstacles,
             starting_position,
         })
     }
 }
 
-fn walk_maze(starting_position: &(usize, usize), map: &[Vec<bool>]) -> usize {
+fn walk_maze(
+    starting_position: &(usize, usize),
+    obstacles: &HashSet<(usize, usize)>,
+    map_height: usize,
+    map_width: usize,
+) -> usize {
     let mut visited_spaces = HashSet::from([*starting_position]);
     let mut current_position = *starting_position;
     let mut movement_direction = Direction::Up;
-
-    let map_height = map.len();
-    let map_width = map[0].len();
 
     loop {
         let movement_delta = get_movement_delta(&movement_direction);
@@ -94,7 +103,7 @@ fn walk_maze(starting_position: &(usize, usize), map: &[Vec<bool>]) -> usize {
                 break;
             }
 
-            if map[next_x][next_y] {
+            if obstacles.contains(&(next_x, next_y)) {
                 // occupied space, rotate
                 movement_direction = next_direction(&movement_direction);
                 continue;
@@ -114,11 +123,13 @@ fn walk_maze(starting_position: &(usize, usize), map: &[Vec<bool>]) -> usize {
 #[must_use]
 pub fn solve_part_1(p: &Problem) -> usize {
     let Problem {
-        map,
+        map_height,
+        map_width,
+        obstacles,
         starting_position,
     } = p;
 
-    walk_maze(starting_position, map)
+    walk_maze(starting_position, obstacles, *map_height, *map_width)
 }
 
 #[cfg(test)]
@@ -141,11 +152,12 @@ mod tests {
     fn test_problem_parsing() {
         let p: Problem = TEST_INPUT.parse().unwrap();
 
-        assert_eq!(p.map.len(), 10);
-        assert_eq!(p.map[0].len(), 10);
-        assert_eq!(p.map[0][0], false);
-        assert_eq!(p.map[0][4], true);
-        assert_eq!(p.map[6][4], false);
+        assert_eq!(p.map_height, 10);
+        assert_eq!(p.map_width, 10);
+        assert_eq!(p.obstacles.contains(&(0, 0)), false);
+        assert_eq!(p.obstacles.contains(&(0, 4)), true);
+        assert_eq!(p.obstacles.contains(&(6, 4)), false);
+        assert_eq!(p.starting_position, (6, 4));
     }
 
     #[test]
